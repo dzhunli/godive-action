cf0748a8ded8ea8790a41092e68c95fb4b92bdfa
dzhunli <dzhunli@gmail.com>
2025-01-06 17:29:13 +0200

    minimal changes for great view

--- src/main.sh
+++ src/main.sh
@@ -31,25 +31,34 @@ check_image_size() {
 	fi
     fi
 }
+get_dive() {
+	echo "::group::Fetching the latest Dive version..." 
+	DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
+	echo "Latest Dive version: $DIVE_VERSION"
+	echo "::endgroup::"
+	echo "::group::Downloading and installing Dive..." 
+	curl -OL https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
+	sudo apt install -qqq ./dive_${DIVE_VERSION}_linux_amd64.deb
+	echo "::endgroup::"   
+}
+check_image() {
+	if [[ "$CONTINUE_ON_FAIL" = "true" ]]; then
+		CI=true dive --ci-config "$CI_CONFIG" "$IMAGE_NAME" || echo -e "\033[1;33m CONTINUE POLICY ENABLED... \033[0m"
+		echo -e "\n\n"
+		echo "#         Pass 'continue_on_fail=false' to fail actions that don't pass the test."
+	else
+		CI=true dive --ci-config "$CI_CONFIG" "$IMAGE_NAME"
+	fi
+}
+
 
 echo "Checking Docker image size..."
 check_image_size "$IMAGE_NAME"
 
-echo "::group::Fetching the latest Dive version..." 
-DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-echo "Latest Dive version: $DIVE_VERSION"
-echo "::endgroup::"
+get_dive
+echo "Checking Docker image size..."
+echo -e -n "\033[1;23m Running Dive analysis on image:\033[0m"
+echo -e -n "\033[1;33m $IMAGE_NAME\033[0m"
+echo -e  "\033[1;23m with CI config: $CI_CONFIG\033[0m"
 
-echo "::group::Downloading and installing Dive..." 
-curl -OL https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
-sudo apt install -qqq ./dive_${DIVE_VERSION}_linux_amd64.deb
-echo "::endgroup::"            
-
-echo -e "\033[1;33m Running Dive analysis on image: $IMAGE_NAME with CI config: $CI_CONFIG \033[0m"
-if [[ "$CONTINUE_ON_FAIL" = "true" ]]; then
-	CI=true dive --ci-config "$CI_CONFIG" "$IMAGE_NAME" || echo -e "\033[1;33m CONTINUE POLICY ENABLED... \033[0m"
-	echo -e "\n\n"
-	echo "#         Pass 'continue_on_fail=false' to fail actions that don't pass the test."
-else
-	CI=true dive --ci-config "$CI_CONFIG" "$IMAGE_NAME"
-fi
+check_image
