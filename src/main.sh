9569e37c6121e8be7eea3c2344878380d3cf4794
dzhunli <dzhunli@gmail.com>
2025-01-06 12:48:26 +0200

    reviewed logic + add continue + fix some in action

--- src/main.sh
+++ src/main.sh
@@ -5,6 +5,7 @@ set -e
 IMAGE_NAME=$1
 CI_CONFIG=$2
 ALLOW_LARGE_IMAGE=$3
+CONTINUE_ON_FAIL=$4
 
 check_image_size() {
     IMAGE_SIZE=$(docker image inspect "$1" --format='{{.Size}}')
@@ -15,10 +16,11 @@ check_image_size() {
 
     if (( $(echo "$IMAGE_SIZE_GB > 1" | bc -l) )); then
         if [[ "$ALLOW_LARGE_IMAGE" != "true" ]]; then
-	        echo -e -n "\033[1;31m Error: The image size exceeds 1 GB. \033[0m"
-		echo "Pass 'allow_large_image=true' to proceed."                                    
+	        echo -e "\033[1;31m Error: The image size exceeds 1 GB. \033[0m"
+		echo -e "\n\n"
+		echo "#		Pass 'allow_large_image=true' to proceed.	 #"
 		exit 1
-        else
+	else
 	        echo -e "\033[1;32m Large image allowed. Continuing... \033[0m"
 	fi
     fi
@@ -27,14 +29,21 @@ check_image_size() {
 echo "Checking Docker image size..."
 check_image_size "$IMAGE_NAME"
 
-echo "Fetching the latest Dive version..."
+echo "::group::Fetching the latest Dive version..." 
 DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
 echo "Latest Dive version: $DIVE_VERSION"
+echo "::endgroup::"
 
-echo "Downloading and installing Dive..."
+echo "::group::Downloading and installing Dive..." 
 curl -OL https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
 sudo apt install -qqq ./dive_${DIVE_VERSION}_linux_amd64.deb
+echo "::endgroup::"            
 
-echo "Running Dive analysis on image: $IMAGE_NAME with CI config: $CI_CONFIG"
-CI=true dive --ci-config "$CI_CONFIG" "$IMAGE_NAME"
-
+echo -e "\033[1;33m Running Dive analysis on image: $IMAGE_NAME with CI config: $CI_CONFIG \033[0m"
+if [[ "$CONTINUE_ON_FAIL" = "true" ]]; then
+	CI=true dive --ci-config "$CI_CONFIG" "$IMAGE_NAME" || echo -e "\033[1;33m CONTINUE POLICY ENABLED... \033[0m"
+	echo -e "\n\n"
+	echo "#         Pass 'continue_on_fail=false' to fail actions that don't pass the test.        #"
+else
+	CI=true dive --ci-config "$CI_CONFIG" "$IMAGE_NAME"
+fi
